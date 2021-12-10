388084f961cbb5b114fd05720d943a3b6f55fd55
Andrew <diver.vlz@gmail.com>
2021-12-10 16:58:03 +0300

    URL namespaces are included

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import home, contacts
 
+app_name = 'mainapp'
+
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
