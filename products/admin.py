81d150c9b54424467d1e29e60781d17b55451925
Andrew <diver.vlz@gmail.com>
2021-12-17 00:34:59 +0300

    add models & register in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import ProductCategory, Product
+
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at', 'updated_at',)
+    list_display_links = ('name',)
+    search_fields = ('name', 'updated_at',)
+    list_filter = ('id', 'name', 'created_at', 'updated_at',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_description', 'price', 'quantity',)
+    search_fields = ('name', 'price',)
+    readonly_fields = ('created_at',)
+    list_display_links = ('name',)
+    list_editable = ('price',)
+    list_filter = ('name', 'updated_at',)
+
+
+admin.site.register(ProductCategory, ProductCategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['pk']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название товара')
+    description = models.TextField(blank=True, verbose_name='Описание товара')
+    short_description = models.CharField(max_length=60, verbose_name='Краткое описание')
+    image = models.ImageField(upload_to='products_images', blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0, verbose_name='Цена товара')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество товара')
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товара'
+        ordering = ['-updated_at']
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
