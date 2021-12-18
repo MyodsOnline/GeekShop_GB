0da7bf9006a6d83c3eda8e74f38358eeb4640d08
diver.vlz@gmail.com <123/877Odu>
2021-12-18 23:40:15 +0300

    create login form

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import test
+from .views import test, login, logout
 
 app_name = 'auth'
 
 urlpatterns = [
-    path('', test, name='test'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
--- authapp/views.py
+++ authapp/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+from authapp.forms import ShopUserLoginForm
 
 
 def test(request):
     return HttpResponse('<h1>Authapp</h1>')
+
+
+def login(request):
+    title = 'Вход'
+
+    login_form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+
+    context = {
+        'title': title,
+        'login_form': login_form,
+    }
+    return render(request, 'authapp/login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
