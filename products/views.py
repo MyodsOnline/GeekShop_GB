4ed3375227fb80f336ee38f8c86e5aa649a410fe
Andrew <diver.vlz@gmail.com>
2021-12-22 19:55:10 +0300

    create views for products

--- products/views.py
+++ products/views.py
@@ -1,24 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import os
 import json
+
+from products.models import ProductCategory, Product
 
 SOURCE_DIR = os.path.dirname(__file__)
 source_file = os.path.join(SOURCE_DIR, 'static/products.json')
 
 
-def products(request):
+def products(request, pk=None):
     title = 'Products'
-    links_menu = [
-        {'href': 'products', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+    links_menu = ProductCategory.objects.all()
     products_json = json.load(open(source_file, encoding='utf-8'))
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        context = {
+            'title': title,
+            'links_menu': links_menu,
+            'products_json': products_json,
+            'products': products,
+            'category': category,
+        }
+        return render(request, 'products/products.html', context)
+
+    same_products = Product.objects.all()[:3]
+    print(same_products)
     context = {
         'title': title,
         'links_menu': links_menu,
-        'products_json': products_json,
+        'same_products': same_products,
     }
     return render(request, 'products/products.html', context)
