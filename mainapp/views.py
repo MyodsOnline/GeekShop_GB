ead4ec871bb261025c6a8cadcba13976528a6579
Andrew Smith <diver.vlz@gmail.com>
2021-12-31 03:43:13 +0300

    carousel on homepage

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,12 +7,19 @@ from basketapp.models import Basket
 def home(request):
     title = 'GeekShop'
     products_list = Product.objects.all()[:4]
+    promo = [
+        {'promo_title': 'Тенденции', 'promo_text': 'Опыт профессионалов', 'promo_slogan': 'Новое качество жизни'},
+        {'promo_title': 'Решительность', 'promo_text': 'Скорее в отрыв!', 'promo_slogan': 'Мировые тенденции'},
+        {'promo_title': 'Образ', 'promo_text': 'Следующий уровень комфорта', 'promo_slogan': 'Советы экспертов'},
+    ]
+
     basket = []
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
     context = {
         'title': title,
         'products_list': products_list,
+        'promo': promo,
         'basket': basket,
     }
     return render(request, 'mainapp/index.html', context)
