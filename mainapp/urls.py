89fc5d5339e93a56359470e4544353c628f6dae6
Andrew <diver.vlz@gmail.com>
2022-01-05 19:36:31 +0300

    a small crutch

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import home, contacts
+from .views import home, contacts, new
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('new/', new, name='new'),
     path('contacts/', contacts, name='contacts'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -22,6 +22,23 @@ def home(request):
     return render(request, 'mainapp/index.html', context)
 
 
+def new(request):
+    title = 'GeekShop'
+    products_list = Product.objects.all()[1:5]
+    promo = Promo.objects.all()
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+    context = {
+        'title': title,
+        'products_list': products_list,
+        'promo': promo,
+        'basket': basket,
+    }
+    return render(request, 'mainapp/index.html', context)
+
+
 def contacts(request):
     title = 'Contacts'
     basket = []
