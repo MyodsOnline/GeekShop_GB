80e4599739ea4270fcfb6c4fb6a45177d9cc21c2
Andrew Smith <diver.vlz@gmail.com>
2022-01-21 15:14:44 +0300

    views for ordersapp created

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -6,19 +6,18 @@ from ordersapp.models import Order, OrderItem
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['created', 'updated', 'status', 'is_active']
         exclude = ('user',)
-        widgets = {
-            'created': forms.TextInput(attrs={'class': 'form-control'}),
-            'updated': forms.TextInput(attrs={'class': 'form-control'}),
-            'status': forms.Select(attrs={'class': 'form-control'}),
-            'is_active': forms.CheckboxInput(attrs={'class': 'form-control'}),
-        }
+
+        def __init__(self, *args, **kwargs):
+            super(OrderItemForm, self).__init__(*args, **kwargs)
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'form-control'
 
 
 class OrderItemForm(forms.ModelForm):
     class Meta:
         model = OrderItem
+        exclude = ()
 
     def __init__(self, *args, **kwargs):
         super(OrderItemForm, self).__init__(*args, **kwargs)
