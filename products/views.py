cc9acbb732df5b12ab0480e5c8f0946cfee7b2d3
Andrew Smith <diver.vlz@gmail.com>
2022-02-06 02:18:43 +0300

    product update when updating a category

--- products/views.py
+++ products/views.py
@@ -1,10 +1,14 @@
 import random
+
+from django.db import connection
+from django.db.models.signals import pre_save
 from django.shortcuts import render, get_object_or_404
 import os
 import json
 from django.conf import settings
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
+from django.dispatch import receiver
 
 from products.models import ProductCategory, Product
 
@@ -139,3 +143,20 @@ def product(request, pk):
         'product': get_product(pk),
     }
     return render(request, 'products/product_detail.html', context)
+
+
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x['sql'], queries))
+    print(f'db_profile {type} for {prefix}')
+    [print(query['sql']) for query in update_queries]
+
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_update_productcategory_save(sender, instance, **kwargs):
+    if instance.pk:
+        if instance.is_active:
+            instance.product_set.update(is_active=True)
+        else:
+            instance.product_set.update(is_active=False)
+
+        db_profile_by_type(sender, 'UPDATE', connection.queries)
