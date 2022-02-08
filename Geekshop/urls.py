8c40dac639161ed4cba5a178a3b229a1ae23ae45
Andrew Smith <diver.vlz@gmail.com>
2022-02-08 19:43:18 +0300

    test for mainppp created

--- Geekshop/urls.py
+++ Geekshop/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
+if settings.DEBUG == True:
     import debug_toolbar
 
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
